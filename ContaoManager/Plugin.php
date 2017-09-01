3dcc7bad83e250f65b8104f533ad38265387a34f
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-09-01 20:02:40 +0200

    Add use statements

--- ContaoManager/Plugin.php
+++ ContaoManager/Plugin.php
@@ -3,6 +3,7 @@
 namespace con4gis\ProjectsBundle\ContaoManager;
 
 use con4gis\ApiBundle\Con4gisApiBundle;
+use con4gis\CoreBundle\con4gisCoreBundle;
 use con4gis\ProjectsBundle\con4gisProjectsBundle;
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
