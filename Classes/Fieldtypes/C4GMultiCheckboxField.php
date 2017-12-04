010b33b69d8a8fcba0b71662836c343cd03d1135
coastforge-cro <claudio.ross@kuestenschmiede.de>
2017-12-04 12:00:11 +0100

    Make sure the createFieldData method always returns an array

--- Classes/Fieldtypes/C4GMultiCheckboxField.php
+++ Classes/Fieldtypes/C4GMultiCheckboxField.php
@@ -257,7 +257,7 @@ class C4GMultiCheckboxField extends C4GBrickField
                 $valueArr[] = trim($key);
             }
         }
-        $fieldData = '';
+        $fieldData = [];
         if(sizeof($valueArr) > 0) {
             if ($this->serializeResult) {
                 $fieldData = serialize($valueArr);
