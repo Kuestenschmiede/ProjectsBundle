9ea47671f93526c864076fec94fce9be08685035
coastforge-rro <raphael.ross@kuestenschmiede.de>
2018-08-29 09:58:34 +0200

    add copy right texts

--- Resources/public/js/C4GDialogDataTables.js
+++ Resources/public/js/C4GDialogDataTables.js
@@ -1,3 +1,14 @@
+/**
+ * con4gis - the gis-kit
+ *
+ * @version   php 7
+ * @package   con4gis
+ * @author    con4gis contributors (see "authors.txt")
+ * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright Küstenschmiede GmbH Software & Design 2011 - 2018
+ * @link      https://www.kuestenschmiede.de
+ */
+
 $(document).ready( function () {
     var tables = document.getElementsByClassName('c4g_datatables');
     var index = 0;
