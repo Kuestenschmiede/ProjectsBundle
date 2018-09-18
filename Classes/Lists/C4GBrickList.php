7c5e03e67fbd9a8a9b65f456d0cbc2f89d6bfcfe
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2018-09-18 21:06:37 +0200

    add align center for datatables

--- Classes/Lists/C4GBrickList.php
+++ Classes/Lists/C4GBrickList.php
@@ -338,6 +338,8 @@ class C4GBrickList
             $additionalClasses = ' c4g_list_align_left';
             if ($column->getAlign() == 'right') {
                 $additionalClasses = ' c4g_list_align_right';
+            } else if ($column->getAlign() == 'center') {
+                $additionalClasses = ' c4g_list_align_center';
             }
             if ($column->isShowSum()) {
                 $additionalClasses .= ' c4g_sum';
