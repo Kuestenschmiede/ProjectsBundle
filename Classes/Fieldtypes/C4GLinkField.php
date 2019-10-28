c5117873cfeb52c8202a29f06e17b2da670f5e0c
rro <raphael.ross@kuestenschmiede.gmbh>
2019-10-28 14:55:23 +0100

    Rework C4GLinkField

--- Classes/Fieldtypes/C4GLinkField.php
+++ Classes/Fieldtypes/C4GLinkField.php
@@ -12,36 +12,30 @@
  */
 namespace con4gis\ProjectsBundle\Classes\Fieldtypes;
 
+use con4gis\CoreBundle\Resources\contao\classes\C4GUtils;
 use con4gis\ProjectsBundle\Classes\Dialogs\C4GBrickDialogParams;
 use con4gis\ProjectsBundle\Classes\Fieldlist\C4GBrickField;
+use Contao\Controller;
 
 class C4GLinkField extends C4GBrickField
 {
-    /**
-     * @return mixed
-     */
-    public function getLinkLabel()
-    {
-        return $this->linkLabel;
-    }
+    protected $linkLabel = '';
+    protected $linkType = self::LINK_TYPE_DEFAULT;
 
-    /**
-     * @param mixed $linkLabel
-     */
-    public function setLinkLabel($linkLabel)
-    {
-        $this->linkLabel = $linkLabel;
-    }
-
+    const LINK_TYPE_DEFAULT = 10;
+    const LINK_TYPE_PHONE = 20;
+    const LINK_TYPE_EMAIL = 30;
 
     /**
      * C4GLinkField constructor.
      */
     public function __construct()
     {
-        $this->setDatabaseField(false);
-        $this->setComparable(false);
-        $this->linkLabel = $this->getLinkLabel();
+        parent::__construct();
+        $this->setDatabaseField(true);
+//        $this->setComparable(false);
+        $this->setEditable(false);
+
     }
 
     /**
@@ -53,27 +47,102 @@ class C4GLinkField extends C4GBrickField
     {
         $id = "c4g_" . $this->getFieldName();
         $required = $this->generateRequiredString($data, $dialogParams);
+
+        $beforeValue = $this->getAddStrBeforeValue();
+        $this->setAddStrBeforeValue('');
+        $afterValue = $this->getAddStrBehindValue();
+        $this->setAddStrBehindValue('');
+
         $value = $this->generateInitialValue($data);
         $result = '';
 
         if ($this->isShowIfEmpty() || !empty($value)) {
-
             $condition = $this->createConditionData($fieldList, $data);
+
+            switch ($this->linkType) {
+                case self::LINK_TYPE_PHONE:
+                    $href = 'tel:'.$value;
+                    break;
+                case self::LINK_TYPE_EMAIL:
+                    $href = 'mailto:'.$value;
+                    break;
+                default:
+                    if ($value !== '') {
+                        if (!C4GUtils::startsWith($value, 'http')) {
+                            $href = 'http://'.$value;
+                        } else {
+                            $href = $value;
+                        }
+                    }
+                    break;
+            }
+
+            $this->linkLabel ? $label = $this->linkLabel : $label = $value;
 
             $result =
                 $this->addC4GField($condition,$dialogParams,$fieldList,$data,
-                '<a ' . $required . ' ' . $condition['conditionPrepare'] . ' id="' . $id . '" class="formdata ' . $id . ' c4g_brick_link" href="' . $value . '">' .$this->getLinkLabel() . '</a>');
+                '<a ' . $required . ' ' . $condition['conditionPrepare'] .
+                ' id="' . $id . '" class="formdata ' . $id . ' c4g_brick_link" href="' .
+                $href . '">' . $beforeValue . $label . $afterValue . '</a>');
         }
         return $result;
     }
 
     /**
-     * Method that will be called in the compareWithDB() in C4GBrickDialog
+     * Public method for creating the field specific list HTML
+     * @param $rowData
+     * @param $content
+     * @return mixed
+     */
+    public function getC4GListField($rowData, $content)
+    {
+        $fieldName = $this->getFieldName();
+
+        $value = $rowData->$fieldName;
+
+        if ($value === '' && !$this->isShowIfEmpty()) {
+            return '';
+        }
+
+        if ($this->getAddStrBeforeValue()) {
+            $value = $this->getAddStrBeforeValue().$value;
+        }
+        if ($this->getAddStrBehindValue()) {
+            $value = $value.$this->getAddStrBehindValue();
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param $linkLabel
+     * @return $this
+     */
+    public function setLinkLabel($linkLabel): C4GLinkField
+    {
+        $this->linkLabel = $linkLabel;
+        return $this;
+    }
+
+    /**
+     * @param int $linkType
+     * @return C4GLinkField
+     */
+    public function setLinkType(int $linkType): C4GLinkField
+    {
+        $this->linkType = $linkType;
+        return $this;
+    }
+
+    /**
      * @param $dbValue
      * @param $dlgvalue
      * @return array
      */
-    public function compareWithDB($dbValues, $dlgValues)
+    public function compareWithDB($dbValue, $dlgvalue)
     {
+        return parent::compareWithDB($dbValue, $dlgvalue);
     }
+
+
 }
