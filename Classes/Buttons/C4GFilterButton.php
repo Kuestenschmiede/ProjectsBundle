66adfd41b3606a1ef80de3858316d433045a1375
rro <raphael.ross@kuestenschmiede.gmbh>
2020-01-15 10:45:59 +0100

    Add title and onclick attributes to filter button

--- Classes/Buttons/C4GFilterButton.php
+++ Classes/Buttons/C4GFilterButton.php
@@ -7,10 +7,14 @@ namespace con4gis\ProjectsBundle\Classes\Buttons;
 class C4GFilterButton
 {
     protected $icon;
+    protected $description;
+    protected $class;
 
-    public function __construct(string $icon)
+    public function __construct(string $icon, string $description, string $class)
     {
         $this->icon = $icon;
+        $this->description = $description;
+        $this->class = $class;
     }
 
     /**
@@ -18,6 +22,9 @@ class C4GFilterButton
      */
     public function getButtonHtml(): string
     {
-        return '<span class="ui-button ui-corner-all">'.$this->icon.'</span>';
+        return '<span class="ui-button ui-corner-all" title="'.$this->description.
+            '" onclick="let element = document.getElementsByClassName(\'c4g_brick_list\')[0];'.
+            'element.classList.toggle(\'filter_'.$this->class.'_parent\');'.
+            'this.classList.toggle(\'c4g_button_pressed\')">'.$this->icon.'</span>';
     }
 }
