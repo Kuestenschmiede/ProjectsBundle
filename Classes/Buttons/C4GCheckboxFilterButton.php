15bc4b5204d3f52e3f691a274c02d7b7316861ec
rro <raphael.ross@kuestenschmiede.gmbh>
2020-01-23 10:27:43 +0100

    Add different styles for checkbox filter button

--- Classes/Buttons/C4GCheckboxFilterButton.php
+++ Classes/Buttons/C4GCheckboxFilterButton.php
@@ -5,8 +5,11 @@ namespace con4gis\ProjectsBundle\Classes\Buttons;
 class C4GCheckboxFilterButton implements C4GFilterButtonInterface
 {
     protected $label;
+    protected $labelChecked = '';
+    protected $labelUnChecked = '';
     protected $description;
     protected $class;
+    protected $style = 'checkbox';
 
     public function __construct(string $label, string $description, string $class)
     {
@@ -20,10 +23,80 @@ class C4GCheckboxFilterButton implements C4GFilterButtonInterface
      */
     public function getButtonHtml(): string
     {
-        return '<span class="checkbox" title="' . $this->description .
-            '" onclick="let element = document.getElementsByClassName(\'c4g_brick_list\');' .
-            'element = element.item(element.length - 1);' .
-            'element.classList.toggle(\'filter_' . $this->class . '_parent\');' .
-            'this.classList.toggle(\'checkbox_checked\')">' . $this->label . '</span>';
+        switch ($this->style) {
+            case 'checkbox':
+                return '<input id="'.$this->label.'" type="checkbox" title="' . $this->description .
+                    '" onclick="let element = document.getElementsByClassName(\'c4g_brick_list\');' .
+                    'element = element.item(element.length - 1);' .
+                    'element.classList.toggle(\'filter_' . $this->class . '_parent\');' .
+                    '">' . '<label for="'.$this->label.'">' . $this->label . '</label>';
+                break;
+            case 'button':
+                return '<span class="ui-button ui-corner-all" title="' . $this->description .
+                    '" onclick="let element = document.getElementsByClassName(\'c4g_brick_list\');' .
+                    'element = element.item(element.length - 1);' .
+                    'element.classList.toggle(\'filter_' . $this->class . '_parent\');' .
+                    'this.classList.toggle(\'c4g_button_pressed\');' .
+                    'if (this.innerHTML === \''.$this->labelUnChecked.'\') {this.innerHTML = \''.$this->labelChecked.'\'}'.
+                    ' else {this.innerHTML = \''.$this->labelUnChecked.'\'}">'. $this->labelUnChecked . '</span>';
+                break;
+            default:
+                return '';
+                break;
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabelChecked(): string
+    {
+        return $this->labelChecked;
+    }
+
+    /**
+     * @param string $labelChecked
+     * @return C4GCheckboxFilterButton
+     */
+    public function setLabelChecked(string $labelChecked): C4GCheckboxFilterButton
+    {
+        $this->labelChecked = $labelChecked;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabelUnChecked(): string
+    {
+        return $this->labelUnChecked;
+    }
+
+    /**
+     * @param string $labelUnChecked
+     * @return C4GCheckboxFilterButton
+     */
+    public function setLabelUnChecked(string $labelUnChecked): C4GCheckboxFilterButton
+    {
+        $this->labelUnChecked = $labelUnChecked;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStyle(): string
+    {
+        return $this->style;
+    }
+
+    /**
+     * @param string $style
+     * @return C4GCheckboxFilterButton
+     */
+    public function setStyle(string $style): C4GCheckboxFilterButton
+    {
+        $this->style = $style;
+        return $this;
     }
 }
