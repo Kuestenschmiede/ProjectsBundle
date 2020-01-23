8d8f4d4891a17dbee5a020d249eeb549e79e10d8
rro <raphael.ross@kuestenschmiede.gmbh>
2020-01-23 15:24:20 +0100

    Add C4GSelectFilterButton

---
+++ Classes/Buttons/C4GSelectFilterButton.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace con4gis\ProjectsBundle\Classes\Buttons;
+
+class C4GSelectFilterButton implements C4GFilterButtonInterface
+{
+    protected $options;
+
+    public function __construct(array $options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @return string
+     */
+    public function getButtonHtml(): string
+    {
+        $options = "<option value=\"\">-</option>";
+        foreach ($this->options as $key => $option) {
+            $options .= "<option value=\"$option\">$option</option>";
+        }
+
+        $classesToRemove = '';
+        foreach ($this->options as $key => $option) {
+            $classesToRemove .= "element.classList.remove('filter_type_" . str_replace(' ', '', $option) . "\_parent');";
+        }
+
+        return '<select onchange="let element = document.getElementsByClassName(\'c4g_brick_list\');' .
+            'element = element.item(element.length - 1);' . $classesToRemove .
+            'element.classList.add(\'filter_type_\' + this.options[this.selectedIndex].value.replace(/\s+/g, \'\') + \'_parent\');">' . $options .
+            '</select>';
+    }
+}
