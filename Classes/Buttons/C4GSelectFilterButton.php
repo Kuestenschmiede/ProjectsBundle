4ae925397f7031036ff88d90c982b7958e2b49c4
rro <raphael.ross@kuestenschmiede.gmbh>
2020-02-25 11:52:22 +0100

    Add C4GSelectFilterButton::label and C4GSelectFilterButton::id

--- Classes/Buttons/C4GSelectFilterButton.php
+++ Classes/Buttons/C4GSelectFilterButton.php
@@ -6,11 +6,15 @@ class C4GSelectFilterButton implements C4GFilterButtonInterface
 {
     protected $options;
     protected $class;
+    protected $label;
+    protected $id;
 
-    public function __construct(array $options, string $class)
+    public function __construct(array $options, string $class, string $label, string $id)
     {
         $this->options = $options;
         $this->class = $class;
+        $this->label = $label;
+        $this->id = $id;
     }
 
     /**
@@ -28,9 +32,9 @@ class C4GSelectFilterButton implements C4GFilterButtonInterface
             $classesToRemove .= "element.classList.remove('filter_" . $this->class . '_' . str_replace(' ', '', $option) . "\_parent');";
         }
 
-        return '<select class="c4g_list_filter" onchange="let element = document.getElementsByClassName(\'c4g_brick_list\');' .
+        return '<span><label for="' . $this->id . '">' . $this->label . '</label></span><span><select id="' . $this->id . '" class="c4g_list_filter" onchange="let element = document.getElementsByClassName(\'c4g_brick_list\');' .
             'element = element.item(element.length - 1);' . $classesToRemove .
             'element.classList.add(\'filter_' . $this->class . '_\' + this.options[this.selectedIndex].value.replace(/\s+/g, \'\') + \'_parent\');">' . $options .
-            '</select>';
+            '</select></span>';
     }
 }
