b1319279e7f8eafbfe58eb8d9fbddf59c039fec1
rro <raphael.ross@kuestenschmiede.gmbh>
2020-03-26 17:40:17 +0100

    Add C4GSelectFilterButton::labelMode

--- Classes/Buttons/C4GSelectFilterButton.php
+++ Classes/Buttons/C4GSelectFilterButton.php
@@ -8,13 +8,15 @@ class C4GSelectFilterButton implements C4GFilterButtonInterface
     protected $class;
     protected $label;
     protected $id;
+    protected $labelMode;
 
-    public function __construct(array $options, string $class, string $label, string $id)
+    public function __construct(array $options, string $class, string $label, string $id, int $labelMode)
     {
         $this->options = $options;
         $this->class = $class;
         $this->label = $label;
         $this->id = $id;
+        $this->labelMode = $labelMode;
     }
 
     /**
@@ -22,7 +24,12 @@ class C4GSelectFilterButton implements C4GFilterButtonInterface
      */
     public function getButtonHtml(): string
     {
-        $options = '<option value="">-</option>';
+        if ($this->labelMode === 1 || $this->labelMode === 2) {
+            $options = '<option value="">'.$this->label.'</option>';
+        } else {
+            $options = '<option value="">-</option>';
+        }
+
         foreach ($this->options as $key => $option) {
             $options .= '<option value="' . str_replace([' ', '/', '.', ',', '-'], '', $option) . "\">$option</option>";
         }
@@ -32,9 +39,17 @@ class C4GSelectFilterButton implements C4GFilterButtonInterface
             $classesToRemove .= "element.classList.remove('filter_" . $this->class . '_' . str_replace([' ', '/', '.', ',', '-'], '', $option) . "\_parent');";
         }
 
-        return '<span><label for="' . $this->id . '">' . $this->label . '</label></span><span><select id="' . $this->id . '" class="c4g_list_filter" onchange="let element = document.getElementsByClassName(\'c4g_brick_list\');' .
+        if ($this->labelMode === 0 || $this->labelMode === 2) {
+            $label = '<label for="' . $this->id . '">' . $this->label . '</label>';
+            $ariaLabel = '';
+        } else {
+            $label = '<label for="' . $this->id . '"></label>';
+            $ariaLabel = ' aria-label="'.$this->label.'"';
+        }
+
+        return '<span>'.$label.'</span><span><select id="' . $this->id . '" class="c4g_list_filter" onchange="let element = document.getElementsByClassName(\'c4g_brick_list\');' .
             'element = element.item(element.length - 1);' . $classesToRemove .
-            'element.classList.add(\'filter_' . $this->class . '_\' + this.options[this.selectedIndex].value.replace(/\s+/g, \'\') + \'_parent\');">' . $options .
+            'element.classList.add(\'filter_' . $this->class . '_\' + this.options[this.selectedIndex].value.replace(/\s+/g, \'\') + \'_parent\');"'.$ariaLabel.'>' . $options .
             '</select></span>';
     }
 }
