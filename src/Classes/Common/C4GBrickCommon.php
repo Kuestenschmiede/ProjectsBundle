172e052b8166b18c024721f0a67bdfdb2852089e
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2022-01-13 10:17:02 +0100

    Fixed uninitialized string offset warning

--- src/Classes/Common/C4GBrickCommon.php
+++ src/Classes/Common/C4GBrickCommon.php
@@ -320,12 +320,17 @@ class C4GBrickCommon
 
     public static function getUUID()
     {
+        $result = '';
         $s = uniqid('', false);
-        //return base_convert($s, 16, 36);
 
-        $hex = substr($s, 0, 13);
-        $dec = $s[13] . substr($s, 15); // skip the dot
-        $result = base_convert($hex, 16, 36) . base_convert($dec, 10, 36);
+        if (!empty($s)) {  //fixed warning
+            $hex = substr($s, 0, 13);
+            if (!empty($s[13])) {
+              $dec = $s[13] . substr($s, 15); // skip the dot
+            }
+
+            $result = base_convert($hex, 16, 36) . base_convert($dec, 10, 36);
+        }
 
         return $result;
     }
