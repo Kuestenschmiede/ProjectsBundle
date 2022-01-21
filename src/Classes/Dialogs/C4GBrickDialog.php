2d0a5cabc1f46359eb7f15420bd120a9bb4fcaae
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2022-01-21 09:11:07 +0100

    Remove headline without text

--- src/Classes/Dialogs/C4GBrickDialog.php
+++ src/Classes/Dialogs/C4GBrickDialog.php
@@ -446,7 +446,7 @@ class C4GBrickDialog
 
         $viewType = $dialogParams->getViewType();
 
-        $headline = $brickCaption . ' ' . $GLOBALS['TL_LANG']['FE_C4G_DIALOG']['HEADLINES_ADD'];
+        $headline = $brickCaption ? $brickCaption . ' ' . $GLOBALS['TL_LANG']['FE_C4G_DIALOG']['HEADLINES_ADD'] : '';
         if ($viewType == C4GBrickViewType::GROUPFORM || C4GBrickViewType::MEMBERFORM || C4GBrickViewType::PUBLICFORM) {
             $headline = $brickCaption;
         }
@@ -455,11 +455,11 @@ class C4GBrickDialog
             $titleStr = $headline;
         } else {
             if (C4GBrickView::isWithoutEditing($viewType)) {
-                $titleStr = $brickCaption . '<span class="state-text state-show"> ' .
-                    $GLOBALS['TL_LANG']['FE_C4G_DIALOG']['HEADLINES_SHOW'] . '</span>';
+                $titleStr = $brickCaption ? $brickCaption . '<span class="state-text state-show"> ' .
+                    $GLOBALS['TL_LANG']['FE_C4G_DIALOG']['HEADLINES_SHOW'] . '</span>' : '';
             } else {
-                $titleStr = $brickCaption . '<span class="state-text state-edit"> ' .
-                    $GLOBALS['TL_LANG']['FE_C4G_DIALOG']['HEADLINES_EDIT'] . '</span>';
+                $titleStr = $brickCaption ? $brickCaption . '<span class="state-text state-edit"> ' .
+                    $GLOBALS['TL_LANG']['FE_C4G_DIALOG']['HEADLINES_EDIT'] . '</span>' : '';
             }
         }
 
