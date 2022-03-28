7e46d91cc71c0fd00bf14efb0d956cfb6a2d9ac0
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2022-03-28 18:02:45 +0200

    hotfix radio group mandatory check

--- src/Classes/Fieldtypes/C4GRadioGroupField.php
+++ src/Classes/Fieldtypes/C4GRadioGroupField.php
@@ -323,7 +323,7 @@ class C4GRadioGroupField extends C4GBrickField
             foreach ($this->getCondition() as $con) {
                 $fieldName = $con->getFieldName();
                 if (!$con->checkAgainstCondition($dlgValues[$fieldName])) {
-                    return false; //todo shouldn't happend, better error handling.
+                    /*return false; *///todo shouldn't happend, better error handling.
                 }
             }
         }
